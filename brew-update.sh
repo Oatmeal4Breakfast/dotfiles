97cf19fecd14e90428e7aa24de686e924f47f6e9
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-06-09 22:13:41 -0400

    feature: add commits

--- brew-update.sh
+++ brew-update.sh
@@ -20,4 +20,11 @@ brew cleanup --prune=all
 echo "dumping out current config"
 brew bundle dump --file="$DOTFILES_DIR/Brewfile" --force --describe
 
+echo "Committing Brewfile..."
+
+cd "$DOTFILES_DIR"
+git add Brewfile
+git dif --cached --quite || git commit -m "chore: update Brewfile"
+git push || echo "git push failed check ssh agent"
+
 echo "Done."
