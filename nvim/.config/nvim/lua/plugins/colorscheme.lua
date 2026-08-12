de7ec0018208127d274b2784ec04a43a2be11860
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-08-11 22:22:33 -0400

    another change to the theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,44 +1,33 @@
 return {
-  -- Load the standard community-favorite gruvbox plugin
   {
-    "ellisonleao/gruvbox.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
-    config = function()
-      -- Force dark theme variant explicitly
-      vim.o.background = "dark"
-
-      -- Optional configuration settings
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "hard", -- Options: "", "soft", "medium", "hard"
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-    end,
+    opts = {
+      flavour = "frappe",
+      background = {
+        dark = "frappe",
+      },
+      integrations = {
+        blink_cmp = true,
+        gitsigns = true,
+        lsp_trouble = true,
+        mason = true,
+        native_lsp = { enabled = true },
+        noice = true,
+        snacks = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
   },
 
   -- Tell LazyVim to activate it globally on startup
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
